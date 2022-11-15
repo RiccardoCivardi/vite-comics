d054afee8787430a25a292900dc3e83a4f09960b
RiccardoCivardi <riccardo.civardi@icloud.com>
2022-11-15 18:10:09 +0100

    [add] AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,10 +6,26 @@ export default {
 
 <template>
   
-  <div>main</div>
+  <main>
+    <div class="container">
+      <p> --> Content goes here</p>
+    </div>
+  </main>
 
 </template>
 
 <style lang="scss" scoped>
+
+  @use '../style/partials/mixin' as *;
+  @use '../style/partials/variables' as *;
+
+  main {
+    height: $header-height;
+    background-color: $secondary-color;
+    font-size: 1.5rem;
+    color: $text-light;
+    font-weight: bold;
+    @include dFlexRow(alignCenter);
+  }
 
 </style>
